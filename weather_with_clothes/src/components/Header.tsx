01631695dd65de9ebf2a153060d9b318d14f843f
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-04-03 15:25:10 +0900

    gbrkekd

--- weather_with_clothes/src/components/Header.tsx
+++ weather_with_clothes/src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Home from './Icons/Home'
 
 export default function Header() {
   return (
-    <>
-      <div className='w-1/3 float-left'><span className='text-fontWhite'>1</span></div>
-      <div className='w-2/3 float-right'>2</div>
-    </>
+    <div className='flex font-bold text-white w-full'>
+      <div className='mr-6'><Home size="medium"/></div>
+      <div className='text-2xl leading-10'>상봉동</div>
+    </div>
   )
 }
---
+++ weather_with_clothes/src/components/Icons/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { IoHomeOutline } from "react-icons/io5";
+
+export default function Home({size}) {
+  return (
+    <IoHomeOutline className='w-10 h-10 bg'/>
+  )
+}

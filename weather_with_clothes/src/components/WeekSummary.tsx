c5827d085b623a323bfac08aadd370480bde4714
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-04-23 22:16:02 +0900

    뽑아내기는 함

--- weather_with_clothes/src/components/WeekSummary.tsx
+++ weather_with_clothes/src/components/WeekSummary.tsx
@@ -7,17 +7,46 @@ export default function WeekSummary() {
   const [weekData, setWeekData] = useState<any>(null); // null로 초기화
   
   useEffect(() => {
-    console.log(data.response.body.items.item[0])
-    setWeekData(data);
+    setWeekData(filterData(data.response.body.items.item[0]));
+    console.log(weekData)
   }, [data, status]); // useEffect will trigger when data or status changes
 
   if(isPending) return <div>Loading...</div>
 
   if(error) return <div>Error</div>
 
-
-
   return (
-    <div className='mt-4 p-2 rounded-2xl border-2'>WeekSummary</div>
+    <div className='mt-4 p-2 rounded-2xl border-2'>
+      {weekData && (
+        <>
+        {
+          <p>{weekData.regId}</p>}
+          <div>
+            {
+              Object.values(weekData).slice(1).map((ele:any, index:any)=> {
+                return (
+                  <>
+                    <span className='mr-2'>{ele}</span>
+                  </>
+                )
+              })  
+            }
+          </div>
+        </>
+      )}
+    </div>
   )
+}
+
+// 데이터 필터링 함수 정의
+function filterData(data) {
+  let filteredData = {};
+  for (const key in data) {
+      // "High" 또는 "Low"를 포함하지 않는 키만 새로운 객체에 할당
+      if (!key.includes("High") && !key.includes("Low")) {
+          filteredData[key] = data[key];
+      }
+  }
+
+  return filteredData;
 }

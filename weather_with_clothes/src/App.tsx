1c18d9be8096c8daad49a05b277a7d33774a8951
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-05-19 10:19:25 +0900

    이게 맞나..?

--- weather_with_clothes/src/App.tsx
+++ weather_with_clothes/src/App.tsx
@@ -16,6 +16,7 @@ import Summary from './components/Summary';
 import TwentyFourWeatherGraph from './components/TwentyFourWeatherGraph';
 import WeekSummary from './components/WeekSummary';
 import { getTwentyHours, threeDaysWeatherInfo } from './Utils/common';
+import * as xlsx from 'xlsx';
 
 function App() {
 	const { isPending, status, data, error, isFetching } = useTodayWeatherInfo(
@@ -23,6 +24,24 @@ function App() {
 	);
 	const [twentyFourHourData, setTwentyFourHourData] = useState<any>(null); // null로 초기화
 	const [threeDaysWeatherData, setThreeDaysWeatherData] = useState<any>(null); // null로 초기화
+	
+	const [file, setFile] = useState(null);
+  const [jsonData, setJsonData] = useState("");
+
+  const handleConvert = () => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e:any) => {
+        const data = e.target.result;
+        const workbook = xlsx.read(data, { type: "binary" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = xlsx.utils.sheet_to_json(worksheet);
+        setJsonData(JSON.stringify(json, null, 2));
+      };
+      reader.readAsBinaryString(file);
+    }
+  };
 
 	useEffect(() => {
 		if (status === 'success') {
@@ -47,6 +66,15 @@ function App() {
 				<Summary />
 				<WeekSummary threeDaysWeatherData={threeDaysWeatherData} />
 			</div>
+			<div>
+      <input
+        type="file"
+        accept=".xls,.xlsx"
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+      <button onClick={handleConvert}>Convert</button>
+      <pre>{jsonData}</pre>
+    </div>
 		</div>
 	);
 }

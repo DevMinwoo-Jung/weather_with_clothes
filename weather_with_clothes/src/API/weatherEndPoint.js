cae3e5b91f5aaeb19ef1d07cb2d53cb8b97c4b6b
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-09-02 22:56:12 +0900

    https로 바꿈

--- weather_with_clothes/src/API/weatherEndPoint.js
+++ weather_with_clothes/src/API/weatherEndPoint.js
@@ -1,10 +1,10 @@
 // 예보구역코드, 발표시각의 조회 조건으로 예보일로부터 3일에서 10일까지 최저/최고기온정보를  조회하는 기능
 // MidFcstInfoService 중기기온조회
 // 이게 필요한가...? 3~10일치를 조회하는건데
-export const MID_TERM_TEMP_END_POINT = "http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?";
+export const MID_TERM_TEMP_END_POINT = "https://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?";
 
 // 조회조건(지점 번호, 시간)으로 종관기상관측일자료 데이터(지점 번호, 온도, 습도 등)를 조회하는 기능
-export const DAILY_FORCAST_END_POINT = "http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?";
+export const DAILY_FORCAST_END_POINT = "https://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?";
 // https://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?
 // serviceKey=cBcLGg%2FAk0eezgRUzbJ9Zi2tdON1kNjGLWg25%2BV7G4tD5EPP%2BnU0nlMRHaaR3XKwSnfdlh%2BVrKvKndteDNNahg%3D%3D
 
@@ -15,7 +15,8 @@ export const TODAY_FORCAST_END_POINT = "https://apis.data.go.kr/1360000/VilageFc
 
 // 예보구역코드, 발표시각의 조회 조건으로 예보일로부터 3일에서 10일까지 육상날씨정보를 조회하는 기능	
 // 이걸로는 오늘 날씨 기온(TMP), 비 구름(SKY), 강수확률(POP) 나타내면 될듯
-export const WEEK_LAND_FORCAST_END_POINT = "http://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst?";
+export const WEEK_LAND_FORCAST_END_POINT = "https://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst?";
 
 // 중기전망조회
-export const WEEK_SUMMARY_END_POINT = "http://apis.data.go.kr/1360000/MidFcstInfoService/getMidFcst?";
+export const WEEK_SUMMARY_END_POINT = "https://apis.data.go.kr/1360000/MidFcstInfoService/getMidFcst?";
+ 

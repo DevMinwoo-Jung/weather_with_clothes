6499173b919d1f199b6d6117a6b6da781dc6ae06
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-12-03 18:40:14 +0900

    이걸 놓쳤다니;

--- weather_with_clothes/vite.config.js
+++ weather_with_clothes/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weather_with_clothes/",
+  base: "https://github.com/DevMinwoo-Jung/weather_with_clothes/tree/main/weather_with_clothes",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 50000,

7548fa5a9e42b333cbd7bc2b4f6641caa1a52d93
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-12-18 14:54:20 +0900

    vite confiog

--- weather_with_clothes/vite.config.js
+++ weather_with_clothes/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weather_with_clothes",
-  plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 50000,
-    outDir: 'dist',
+    sourcemap: false,
   },
 })

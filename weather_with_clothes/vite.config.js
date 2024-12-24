318cf470aa0dc50e865b74e66ac38ae5ac46ef75
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-12-24 18:50:39 +0900

    config

--- weather_with_clothes/vite.config.js
+++ weather_with_clothes/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      react: 'react',
+      'react-dom': 'react-dom',
+    },
   },
 })
